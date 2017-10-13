6398eec436750ad5108ac365fd8fae7101d147da
juanjose jaquez herrera <juanjose jaquez herrera>
2017-10-12 22:24:15 -0500

    Se agregaron las pruebas de los metodos consultar

--- Software/SMV/test/BDTest.java
+++ Software/SMV/test/BDTest.java
@@ -5,6 +5,7 @@
  */
 
 import java.sql.ResultSet;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -25,7 +26,7 @@ public class BDTest {
     /**
      * Test of Conectar method, of class BD.
      */
-    @Test
+    //@Test
     public void testConectar() throws Exception {
         System.out.println("Conectar");
         BD mBd = new BD();
@@ -38,7 +39,7 @@ public class BDTest {
     /**
      * Test of agregarVehiculo method, of class BD.
      */
-    @Test
+    //@Test
     public void testAgregarVehiculo() throws Exception {
         vehiculo mVehiculo = new vehiculo();
         BD mBd = new BD();
@@ -54,6 +55,128 @@ public class BDTest {
         boolean esperado = true;
         boolean resultado = mBd.agregarVehiculo(mVehiculo);
         assertEquals(esperado, resultado);
+    }
+    /**
+     * Test of ConsultaGeneral method, of class BD.
+     */
+    @Test
+    public void testConsultaGeneral() throws Exception {
+        System.out.println("ConsultaGeneral");
+        BD mBD = new BD();
+        mBD.Conectar();
+        int esperado = 8;
+        List<vehiculo> result = mBD.ConsultaGeneral();
+        assertEquals(esperado, result.size());
+    }
+
+    /**
+     * Test of ConsultaPorMarca method, of class BD.
+     */
+    @Test
+    public void testConsultaPorMarca() throws Exception {
+        System.out.println("ConsultaPorMarca");
+        vehiculo mVehiculo = new vehiculo();
+        mVehiculo.setMarca("Ford");
+        BD mBD = new BD();
+        mBD.Conectar();
+        int esperado = 3;
+        List<vehiculo> result = mBD.ConsultaPorMarca(mVehiculo);
+        assertEquals(esperado, result.size());
+    }
+
+    /**
+     * Test of ConsultaPorModelo method, of class BD.
+     */
+    @Test
+    public void testConsultaPorModelo() throws Exception {
+        System.out.println("ConsultaPorModelo");
+        vehiculo mVehiculo = new vehiculo();
+        mVehiculo.setModelo("sepa");
+        BD mBD = new BD();
+        mBD.Conectar();
+        int esperado = 1;
+        List<vehiculo> result = mBD.ConsultaPorModelo(mVehiculo);
+        assertEquals(esperado, result.size());
+    }
+
+    /**
+     * Test of ConsultaPorAnio method, of class BD.
+     */
+    @Test
+    public void testConsultaPorAnio() throws Exception {
+        System.out.println("ConsultaPorAnio");
+        vehiculo mVehiculo = new vehiculo();
+        mVehiculo.setAnio(1987);
+        BD mBD = new BD();
+        mBD.Conectar();
+        int esperado = 2;
+        List<vehiculo> result = mBD.ConsultaPorAnio(mVehiculo);
+        assertEquals(esperado, result.size());
+    }
+
+    /**
+     * Test of ConsultaMixta method, of class BD.
+     */
+    @Test
+    public void testConsultaMixta() throws Exception {
+        System.out.println("ConsultaMixta");
+        vehiculo mVehiculo = new vehiculo();
+        mVehiculo.setMarca("Nissan");
+        mVehiculo.setModelo("sepa");
+        mVehiculo.setAnio(1986);
+        BD mBD = new BD();
+        mBD.Conectar();
+        int esperado = 1;
+        List<vehiculo> result = mBD.ConsultaMixta(mVehiculo);
+        assertEquals(esperado, result.size());
+    }
+
+    /**
+     * Test of ConsultaMarcaModelo method, of class BD.
+     */
+    @Test
+    public void testConsultaMarcaModelo() throws Exception {
+        System.out.println("ConsultaMarcaModelo");
+        vehiculo mVehiculo = new vehiculo();
+        mVehiculo.setMarca("Chevrolet");
+        mVehiculo.setModelo("pick up");
+        BD mBD = new BD();
+        mBD.Conectar();
+        int esperado = 1;
+        List<vehiculo> result = mBD.ConsultaMarcaModelo(mVehiculo);
+        assertEquals(esperado, result.size());
+    }
+
+    /**
+     * Test of ConsultaMarcaAnio method, of class BD.
+     */
+    @Test
+    public void testConsultaMarcaAnio() throws Exception {
+        System.out.println("ConsultaMarcaAnio");
+        vehiculo mVehiculo = new vehiculo();
+        mVehiculo.setMarca("Ford");
+        mVehiculo.setAnio(1987);
+        BD mBD = new BD();
+        mBD.Conectar();
+        int esperado = 1;
+        List<vehiculo> result = mBD.ConsultaMarcaAnio(mVehiculo);
+        assertEquals(esperado, result.size());;
+    }
+
+    /**
+     * Test of ConsultaModeloAnio method, of class BD.
+     */
+    @Test
+    public void testConsultaModeloAnio() throws Exception {
+        System.out.println("ConsultaModeloAnio");
+        vehiculo mVehiculo = new vehiculo();
+        mVehiculo.setModelo("pick up");
+        mVehiculo.setAnio(2000);
+        BD mBD = new BD();
+        mBD.Conectar();
+        int esperado = 1;
+        List<vehiculo> result = mBD.ConsultaModeloAnio(mVehiculo);
+        assertEquals(esperado, result.size());
     }
     
 }
